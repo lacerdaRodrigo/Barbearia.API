7e7be25ee1c8e3fcc24fc2e6fe911cc46adc5814
lacerdaRodrigo <lacerdaaa.rodrigo@gmail.com>
2024-07-29 21:37:23 -0300

    criando front e pegando api

---
+++ Front-Barbearia/Front-Barbearia/NovaPasta/Servicos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace FrontBarbearia.NovaPasta
+{
+    public class Servicos
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string category { get; set; }
+        public double price { get; set; }
+        public double discountPercentage { get; set; }
+        public double rating { get; set; }
+        public int stock { get; set; }
+        public List<string> tags { get; set; }
+        public string brand { get; set; }
+        public string sku { get; set; }
+        public int weight { get; set; }
+
+
+    }
+}
+
+
